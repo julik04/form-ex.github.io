f64b6d75d80aed39d45c115de63c8841fa3a7cd1
julik04 <116831968+julik04@users.noreply.github.com>
2024-06-29 00:52:49 +0300

    added css

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function validateForm(){
     const name = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    const form = document.getElementById('form').value;
     const email = document.getElementById('email').value;
-    const error = document.getElementById('error');
+    const errorMessage = document.getElementById('errorMessage');
 
     const regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
 
     if(!regexPassword.test(password)){
-        errorMessage.innerHTML = 'Пароль должен содержать минимум 6 символов, одну заглавную и одну строчную буквы и цифру'
+        errorMessage.innerHTML = 'Пароль должен содержать минимум 6 символов, одну заглавную и одну строчную буквы и цифру';
+        return false;
     }
-    alert('Данные отправлены');
     errorMessage.innerHTML = '';
+    alert('Данные отправлены');
     return true;
 }
 
